b50ab272123ac8995db5a9e85115439d44b07493
horvathdanielgabor <horvathdanielgabor@turr.hu>
2025-04-29 12:29:36 +0200

    FileIO animatronic upload function.

--- DA_NiEL/Animatronic.cs
+++ DA_NiEL/Animatronic.cs
@@ -10,16 +10,23 @@ namespace DA_NiEL
     internal class Animatronic
     {
         public string Name;
-        public int Steps;
+        public int StepsMax;
         public string Letter;
-        public List<Image> Jumpscare_ls = new List<Image>();
+        public List<Image> Jumpscare_ls;
 
-        public Animatronic(string name, string steps, string letter, List<Image> jump)
+        public Animatronic(string name, string letter, string steps)
         {
             Name = name;
-            Steps = Convert.ToInt32(steps);
             Letter = letter;
-            Jumpscare_ls = jump;
+            StepsMax = Convert.ToInt32(steps);
+        }
+
+        public void jumpscAdd(string Name, List<Image> jumps)
+        {
+            for (int i = 0; i < jumps.Count; i++)
+            {
+                Jumpscare_ls[int.Parse(Name)] = jumps[i];
+            }
         }
     }
 }
--- DA_NiEL/FileIO.cs
+++ DA_NiEL/FileIO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,62 @@ namespace DA_NiEL
 {
     internal class FileIO
     {
-        static private string filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "\\Load\\", "loadfile.txt");
+        static private string k_filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "\\k_jumpscare\\", "k_0.png");
 
-        static public List<Animatronic> Beolvasas ()
+        static public List<Animatronic> Beolvasas()
         {
+            List<Animatronic> animatronics = new List<Animatronic>();
 
+            try
+            {
+                StreamReader sr = new StreamReader("animatronics.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    string name = line.Split(';')[0];
+
+                    string letter;
+                    try
+                    {
+                        letter = line.Split(';')[1];
+                    }
+                    catch (FormatException)
+                    {
+                        letter = name[0].ToString();
+                    }
+
+                    string maxstep = line.Split(';')[2];
+
+                    Animatronic newone = new Animatronic(name, letter, maxstep);
+
+                    animatronics.Add(newone);
+
+                    int newindex = animatronics.IndexOf(newone);
+
+                    List<Image> jumpscare = JumpscareUpload();
+
+                    animatronics[newindex].jumpscAdd(name, jumpscare);
+                }
+
+                sr.Close();
+            }
+            catch (IOException)
+            {
+
+            }
+
+            return animatronics;
+        }
+
+        private static List<Image> JumpscareUpload()
+        {
+            List <Image> jumpscare = new List<Image>();
+
+
+
+            return jumpscare;
         }
     }
 }
